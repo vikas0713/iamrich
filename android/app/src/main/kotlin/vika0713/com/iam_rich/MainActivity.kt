c8b21a1ff9caf60e2bf28938284edcd516325a62
Vikas Verma <vikas.verma@helloalfred.com>
2023-05-04 11:30:12 +0530

    Initial Commit

---
+++ android/app/src/main/kotlin/vika0713/com/iam_rich/MainActivity.kt
@@ -0,0 +1,6 @@
+package vika0713.com.iam_rich
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+      MaterialApp(
+          home: Scaffold(
+            appBar: AppBar(
+              title: const Text('I am Rich'),
+              backgroundColor: Colors.blueGrey[900],
+            ),
+            backgroundColor: Colors.blueGrey[600],
+            body: const Center(
+              child: Image(
+                image: AssetImage(('images/diamond.jpeg'))
+              ),
+            )
+          ),
+      )
+  );
+}
